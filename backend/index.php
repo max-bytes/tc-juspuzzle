0bbb01aefe0c6518c151d0d4a64996d6488f0c99
Maximilian Csuk <maximilian.csuk@max-bytes.com>
2024-10-29 10:07:25 +0100

    bugfix

--- backend/index.php
+++ backend/index.php
@@ -104,10 +104,10 @@ $app->post('/start', function (Request $request, Response $response, array $args
 $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000/, https://max-bytes.github.io/')
+            ->withHeader('Access-Control-Allow-Origin', 'http://mysite')
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
